d068d286127c624ed2436915961f15300da97bbe
daneeskripter <imdanee@seznam.cz>
2024-01-25 21:28:28 +0100

    api & json handling

--- app/src/main/java/dev/daneeskripter/cernyrobin/HomeFragment.kt
+++ app/src/main/java/dev/daneeskripter/cernyrobin/HomeFragment.kt
@@ -5,6 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.beust.klaxon.Klaxon
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import io.ktor.client.call.body
+import io.ktor.client.statement.bodyAsChannel
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.decodeFromStream
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +43,68 @@ class HomeFragment : Fragment() {
         }
     }
 
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        @Serializable
+        data class VideoInfo(
+            @SerialName("title") val title: String,
+            @SerialName("author_name") val authorName: String,
+            @SerialName("author_url") val authorUrl: String,
+            @SerialName("type") val type: String,
+            @SerialName("height") val height: Int,
+            @SerialName("width") val width: Int,
+            @SerialName("version") val version: String,
+            @SerialName("provider_name") val providerName: String,
+            @SerialName("provider_url") val providerUrl: String,
+            @SerialName("thumbnail_height") val thumbnailHeight: Int,
+            @SerialName("thumbnail_width") val thumbnailWidth: Int,
+            @SerialName("thumbnail_url") val thumbnailUrl: String,
+            @SerialName("html") val html: String,
+            @SerialName("description") val description: String
+        )
+
+        @Serializable
+        data class Video(
+            @SerialName("answers") val answers: String,
+            @SerialName("transcript") val transcript: String,
+            @SerialName("language") val language: String,
+            @SerialName("video_info") val videoInfo: VideoInfo,
+            @SerialName("video_url") val videoUrl: String
+
+        )
+
+        @Serializable
+        data class AutoKafka(
+            @SerialName("code") val code: Int,
+            @SerialName("message") val message: String,
+            @SerialName("list") val list: Map<String, Video>
+        )
+
+        val linearLayout : LinearLayout = view.findViewById(R.id.videosLayout)
+      GlobalScope.launch(Dispatchers.Main) {
+            val result = APIMethods().getRequest("https://api.cernyrob.in/kafka/list").bodyAsText()
+          try {
+              val jsonResult = Json { ignoreUnknownKeys = true }.decodeFromString<AutoKafka>(result)
+              jsonResult.list.forEach {
+                  val title : TextView = TextView(requireContext())
+                  title.text = "Název: ${it.value.videoInfo.title}\nURL: ${it.value.videoUrl}"
+                  linearLayout.addView(title)
+              }
+          } catch (e: Exception) {
+              e.printStackTrace()
+          }
+        }
+
+        return view
     }
 
     companion object {
