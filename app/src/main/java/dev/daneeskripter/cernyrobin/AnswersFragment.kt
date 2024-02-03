34d06802467a2c99b799644fbd589afe76097560
daneeskripter <imdanee@seznam.cz>
2024-02-03 18:21:04 +0100

    questions and answers added

--- app/src/main/java/dev/daneeskripter/cernyrobin/AnswersFragment.kt
+++ app/src/main/java/dev/daneeskripter/cernyrobin/AnswersFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.serialization.json.Json
 
@@ -17,10 +19,31 @@ class AnswersFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_answers, container, false)
         val videoData = arguments?.getString("video")
         val video = Json.decodeFromString(Video.serializer(), videoData ?: "")
-        val text = view.findViewById<TextView>(R.id.testText)
+        val title = view.findViewById<TextView>(R.id.answers_title)
+        title.text = video.videoInfo.title
 
-        text.text = video.videoInfo.title
-
+        val answersLayout = view.findViewById<LinearLayout>(R.id.answersLayout)
+        val questions = video.videoInfo.description.replace("\r\n", "\n").split(":\n")[1].trim().split("\n")
+        val answers = video.answers.replace("\r\n", "\n").trim().split("\n")
+        val backButton = view.findViewById<ImageButton>(R.id.btnBack)
+            for (i in 0..answers.size - 1) {
+                val answerBox = layoutInflater.inflate(R.layout.answer_template, null)
+                val question = answerBox.findViewById<TextView>(R.id.questionText)
+                val answer = answerBox.findViewById<TextView>(R.id.answerText)
+                if (i > 9) {
+                    question.text = "Chyba formatování"
+                } else {
+                    question.text = questions[i]
+                }
+                answer.text = answers[i]
+                answersLayout.addView(answerBox)
+            }
+        backButton.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, HomeFragment())
+                .addToBackStack(null)
+                .commit()
+        }
         return view
     }
 
